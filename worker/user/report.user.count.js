768f6ed9fb6e86dfa45ea1e57f5b70617744d9a2
akinmaurice <akinmaurice@gmail.com>
2019-06-25 10:00:14 +0100

    Kill child process after executing script

--- worker/user/report.user.count.js
+++ worker/user/report.user.count.js
@@ -2,11 +2,15 @@ const { fork } = require('child_process');
 
 
 const reportUserCount = () => {
+    logger.debug('Starting Child Process');
     const getUserCount = fork(`${__dirname}/../../app/scripts/user/user.count.js`);
     getUserCount.send('start');
     getUserCount.on('message', (result) => {
         const { count } = result;
         logger.info(`Total number of Users: ${count}`);
+        logger.debug('Killing Child Process');
+        getUserCount.kill();
+        logger.debug('Child Process Killed');
     });
 };
 
