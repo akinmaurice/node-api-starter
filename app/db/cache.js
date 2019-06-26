c5416b9b1109e5a3564d4b7ddda5106f541ec30a
akinmaurice <akinmaurice@gmail.com>
2019-06-26 09:59:51 +0100

    use promise for redis lib
    Add tests

--- app/db/cache.js
+++ app/db/cache.js
@@ -1,37 +1,34 @@
 const redisClient = require('../../lib/redis');
 
 
-const saveKey = (arg, key, data) => new Promise(((resolve, reject) => {
-    redisClient.hset(arg, key, JSON.stringify(data), (err, resp) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve(resp);
-    });
+const saveKey = (arg, key, data) => new Promise((async(resolve, reject) => {
+    try {
+        await redisClient.hsetAsync(arg, key, JSON.stringify(data));
+        resolve(true);
+    } catch (e) {
+        reject(e);
+    }
 }));
 
 
-const getKey = (arg, key = '') => new Promise(((resolve, reject) => {
-    redisClient.hget(arg, key, (err, result) => {
-        if (err) {
-            reject(err);
-            return;
-        }
+const getKey = (arg, key = '') => new Promise((async(resolve, reject) => {
+    try {
+        const result = await redisClient.hgetAsync(arg, key);
         const data = JSON.parse(result);
         resolve(data);
-    });
+    } catch (e) {
+        reject(e);
+    }
 }));
 
 
-const removeKey = (arg, key = '') => new Promise(((resolve, reject) => {
-    redisClient.hdel(arg, key, (err, result) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve(result);
-    });
+const removeKey = (arg, key = '') => new Promise((async(resolve, reject) => {
+    try {
+        await redisClient.hdelAsync(arg, key);
+        resolve(true);
+    } catch (e) {
+        reject(e);
+    }
 }));
 
 
