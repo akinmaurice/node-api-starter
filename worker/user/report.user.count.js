1f24ccc1114354c4e08cbbe403eabfeb999e4043
akinmaurice <akinmaurice@gmail.com>
2019-07-04 07:47:36 +0100

    catch child process exit

--- worker/user/report.user.count.js
+++ worker/user/report.user.count.js
@@ -3,14 +3,21 @@ const { fork } = require('child_process');
 
 const reportUserCount = () => {
     logger.debug('Starting Child Process');
+
     const getUserCount = fork(`${__dirname}/../../app/scripts/user/user.count.js`);
+
     getUserCount.send('start');
+
     getUserCount.on('message', (result) => {
         const { count } = result;
         logger.info(`Total number of Users: ${count}`);
         logger.debug('Killing Child Process');
         getUserCount.kill();
         logger.debug('Child Process Killed');
+    });
+
+    getUserCount.on('exit', (error) => {
+        logger.error(`Report User Count Child Process Error ${error}`);
     });
 };
 
