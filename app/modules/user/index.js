9b73f4e4a4bcd77362d66b114e0761307d374d7a
akinmaurice <akinmaurice@gmail.com>
2019-07-07 22:44:23 +0100

    Add module to verify password reset

--- app/modules/user/index.js
+++ app/modules/user/index.js
@@ -3,11 +3,13 @@ const register = require('./register');
 const activateAccount = require('./activate.user');
 const resendActivationCode = require('./resend.activation');
 const updatePassword = require('./update.password');
+const verifyPasswordReset = require('./verify.password.reset');
 
 module.exports = {
     login,
     register,
     activateAccount,
     resendActivationCode,
-    updatePassword
+    updatePassword,
+    verifyPasswordReset
 };
