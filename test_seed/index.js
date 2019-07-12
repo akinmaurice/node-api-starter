3bb501cdf177f4450ef09ad2bdad793a2e215e41
akinmaurice <akinmaurice@gmail.com>
2019-07-12 18:22:35 +0100

    update test data seed

--- test_seed/index.js
+++ test_seed/index.js
@@ -1,9 +1,16 @@
 const moment = require('moment');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const faker = require('faker');
+const loggerInit = require('../config/logger');
 const Helpers = require('../app/helpers');
 const query = require('../app/sql');
 const db = require('../lib/database');
 
-const users = [
+const logger = loggerInit('development');
+const fakeUsersCount = 100;
+const limit = 10;
+
+const arr = [
     {
         username: 'testusername',
         email: 'test@gmail.com',
@@ -25,22 +32,111 @@ const users = [
         date_of_birth: '1990-01-01'
     }
 ];
-const password = 'testpasswordscript';
-const timestamp = moment();
+const password = 'testPassword';
 
-const setupTestDB = async() => {
-    const passwordData = await Helpers.Password.hashUserPassword(password);
-    const { salt, hash } = passwordData;
-    await db.tx((t) => {
-        // eslint-disable-next-line array-callback-return
-        const queries = users.map((user) => {
-            const { username, email, date_of_birth } = user;
-            t.one(query.UserSql.createUser, [ username, email, hash,
-                salt, date_of_birth, true, timestamp, timestamp ]);
-        });
-        return t.batch(queries);
+
+const insertUser = async(user) => new Promise(async(resolve, reject) => {
+    try {
+        const timestamp = moment();
+        const {
+            username, email, date_of_birth, hash, salt
+        } = user;
+        await db.one(query.UserSql.createUser, [ username, email, hash,
+            salt, date_of_birth, true, timestamp, timestamp ]);
+        resolve(true);
+    } catch (e) {
+        reject(e);
+    }
+});
+
+
+const processUsers = (users) => new Promise(async(resolve, reject) => {
+    const requests = users.map(async(record) => {
+        await insertUser(record)
+            .catch(e => logger.error(`Error creating ${record} - ${e}`));
     });
+    await Promise.all(requests)
+        .catch(e => {
+            logger.error('Error in sending email for the');
+            reject(e);
+        });
+    resolve();
+});
+
+
+const batchUsersFetch = () => new Promise(async(resolve, reject) => {
+    try {
+        const count = fakeUsersCount;
+        const batches = Math.ceil((count) / (limit));
+        logger.info('[Seed Test DB Job]: Total Records to Process ', count);
+        logger.info(`[Seed Test DB Job]: Running in ${batches} Batch(es)`);
+        const { hash, salt } = await Helpers.Password.hashUserPassword(password);
+        let page = 1;
+        let recordsExist = true;
+        const date = faker.date.between('1970-01-01', '2000-12-31');
+        while (recordsExist) {
+            const users = [];
+            logger.info(`[Seed Test DB Job]: Processing records for Batch ${page}`);
+            for (let i = 1; i <= limit; i += 1) {
+                const user = {
+                    email: faker.internet.email(),
+                    username: faker.internet.userName(),
+                    date_of_birth: moment(date).format('YYYY-MM-DD'),
+                    hash,
+                    salt
+                };
+                users.push(user);
+            }
+            // eslint-disable-next-line no-await-in-loop
+            await processUsers(users);
+            page += 1;
+            if (page === batches + 1) {
+                logger.info('[Seed Test DB Job]: No more records to process');
+                logger.info(`[Seed Test DB Job]: Total records processed ${count}`);
+                recordsExist = false;
+                resolve(page);
+                return;
+            }
+        }
+    } catch (e) {
+        reject(e);
+        logger.error(e);
+    }
+});
+
+
+const batchUsersReal = () => new Promise(async(resolve, reject) => {
+    try {
+        const { hash, salt } = await Helpers.Password.hashUserPassword(password);
+        const date = faker.date.between('1970-01-01', '2000-12-31');
+        const realUsers = [];
+        arr.forEach((element) => {
+            const user = element;
+            user.salt = salt;
+            user.hash = hash;
+            user.date_of_birth = date;
+            realUsers.push(user);
+        });
+        processUsers(realUsers);
+        resolve(true);
+    } catch (e) {
+        reject(e);
+    }
+});
+
+const runBatchProcess = async() => {
+    try {
+        logger.debug(`Seed Test DB Job Starting @ ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+        await batchUsersReal();
+        await batchUsersFetch();
+        logger.debug(`Seed Test DB Job Ending @ ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+        process.exit();
+    } catch (e) {
+        logger.error(`Seed Test DB Job Ending @ ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+        logger.error(e);
+        process.exit();
+    }
 };
 
 
-setupTestDB();
+runBatchProcess();
