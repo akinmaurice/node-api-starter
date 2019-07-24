4d0854c38799ee126b855862a06255afd4c18713
akinmaurice <akinmaurice@gmail.com>
2019-07-24 07:40:04 +0100

    Add documentation

--- app/db/cache.js
+++ app/db/cache.js
@@ -1,6 +1,15 @@
 const redisClient = require('../../lib/redis');
 
 
+/**
+ * @async
+ * @description This function saves data to redis cache
+ * @param arg {string} cache name to save data to
+ * @param key {string} key identifying the data to save
+ * @param data {Object} data to save to cache
+ * @returns {Boolean}  true if data is saved
+ * @throws {e} Error Response
+ */
 const saveKey = (arg, key, data) => new Promise((async(resolve, reject) => {
     try {
         await redisClient.hsetAsync(arg, key, JSON.stringify(data));
@@ -11,6 +20,14 @@ const saveKey = (arg, key, data) => new Promise((async(resolve, reject) => {
 }));
 
 
+/**
+ * @async
+ * @description This function retrieves data from redis cache
+ * @param arg {string} cache name to fetch data from
+ * @param key {string} key identifying the data to fetch
+ * @returns {Object}  The data retrieved from the cache
+ * @throws {e} Error Response
+ */
 const getKey = (arg, key = '') => new Promise((async(resolve, reject) => {
     try {
         const result = await redisClient.hgetAsync(arg, key);
@@ -22,6 +39,14 @@ const getKey = (arg, key = '') => new Promise((async(resolve, reject) => {
 }));
 
 
+/**
+ * @async
+ * @description This function deletes data from redis cache
+ * @param arg {string} cache name to delete data from
+ * @param key {string} key identifying the data to delete
+ * @returns {Boolean}  true if data is deleted
+ * @throws {e} Error Response
+ */
 const removeKey = (arg, key = '') => new Promise((async(resolve, reject) => {
     try {
         await redisClient.hdelAsync(arg, key);
